c0ffa9a66eed208fbd0ff966328e358f9a54d2ae
Bailey Hollingsworth <bailey.a.hollingsworth-1@ou.edu>
2016-12-20 13:33:56 -0600

    Version bump

--- src/main/java/com/baileyhollingsworth/main/core/EventTweaks.java
+++ src/main/java/com/baileyhollingsworth/main/core/EventTweaks.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class EventTweaks
 {
     public static final String MODID = "event_tweaks";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String URL = "https://raw.githubusercontent.com/baileyholl/Event-Tweaks/master/Version";
 
     @SidedProxy
